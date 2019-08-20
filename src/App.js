e11684084bb6d865b25b655d424981f3c9fe17e6
João Fernando Ribeiro <joao.amorim@usp.br>
2019-08-20 12:51:24 -0300

    FIrst Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export default class App extends Component {
   render() {
     return (
-      <div> Hello from App </div>
+      <div>
+        <div className='text-warning display-2'> Hello from App </div>
+        <FontAwesomeIcon icon="coffee" size="2x" />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faCheckSquare} from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faCoffee, faCheckSquare)
 
 ReactDOM.render(<App />, document.getElementById('root'));
